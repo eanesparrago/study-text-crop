1a951a1bfdad3241fb42a3c6b0aeac833ac01bc5
LJEsp <esparragoleandroj@gmail.com>
2019-01-15 18:08:53 +0800

    First commit

---
+++ docs/precache-manifest.bf04408193c45141d8b64c0ef6c3ce60.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d7e99e058151ffd9f655",
+    "url": "/static/js/main.d7e99e05.chunk.js"
+  },
+  {
+    "revision": "472721ecf88920ed206c",
+    "url": "/static/js/1.472721ec.chunk.js"
+  },
+  {
+    "revision": "60cb77a9ea8098f2c437fa292b02c0c4",
+    "url": "/index.html"
+  }
+];
--- src/App.js
+++ src/App.js
@@ -1,26 +1,138 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import styled, { css, createGlobalStyle } from "styled-components";
+
+const textCrop = (
+  lineHeight = 1.3,
+  topAdjustment = 0,
+  bottomAdjustment = 0
+) => {
+  const topCrop = 8;
+  const bottomCrop = 8;
+  const cropFontSize = 36;
+  const cropLineHeight = 1.2;
+
+  const dynamicTopCrop =
+    (topCrop + (lineHeight - cropLineHeight) * (cropFontSize / 2)) /
+    cropFontSize;
+
+  const dynamicBottomCrop =
+    (bottomCrop + (lineHeight - cropLineHeight) * (cropFontSize / 2)) /
+    cropFontSize;
+
+  const marginBottom = `-${dynamicTopCrop + topAdjustment}em`;
+
+  const marginTop = `-${dynamicBottomCrop + bottomAdjustment}em`;
+
+  return css`
+    line-height: ${lineHeight};
+
+    &::before,
+    &::after {
+      content: "";
+      display: block;
+      height: 0;
+      width: 0;
+    }
+
+    &::before {
+      margin-bottom: ${marginBottom};
+    }
+
+    &::after {
+      margin-top: ${marginTop};
+    }
+  `;
+};
+
+const GlobalStyle = createGlobalStyle`
+ * {
+   box-sizing: border-box;
+ }
+
+ html, body, #root {
+   margin: 0;
+   padding: 0
+ }
+
+ body {
+   font-family: "Muli", sans-serif;
+   padding: 1em;
+ }
+`;
+
+const StyledApp = styled.div`
+  .container {
+    border: 1px solid magenta;
+    width: 50%;
+
+    :not(:last-child) {
+      margin-bottom: 1em;
+    }
+  }
+
+  .text-crop {
+    ${textCrop(undefined, 0, 0)};
+  }
+
+  .text-1 {
+    font-size: 1em;
+  }
+
+  .text-2 {
+    font-size: 2em;
+  }
+
+  .text-3 {
+    font-size: 4em;
+  }
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <StyledApp>
+        <GlobalStyle />
+
+        <h1>Without text crop</h1>
+        <div className="container">
+          <span className="text-1">
+            Laborum sint deserunt qui ex consequat proident voluptate et
+            excepteur elit quis.
+          </span>
+        </div>
+
+        <div className="container">
+          <span className="text-2">
+            Nulla laborum nostrud officia id nulla eiusmod minim ea.
+          </span>
+        </div>
+
+        <div className="container">
+          <span className="text-3">
+            Nulla laborum nostrud officia id nulla eiusmod minim ea.
+          </span>
+        </div>
+
+        <h1>With text crop</h1>
+        <div className="container">
+          <span className="text-1 text-crop">
+            Laborum sint deserunt qui ex consequat proident voluptate et
+            excepteur elit quis.
+          </span>
+        </div>
+
+        <div className="container">
+          <span className="text-2 text-crop">
+            Voluptate anim ad nisi nulla reprehenderit laborum.
+          </span>
+        </div>
+
+        <div className="container">
+          <span className="text-3 text-crop">
+            Voluptate anim ad nisi nulla reprehenderit laborum.
+          </span>
+        </div>
+      </StyledApp>
     );
   }
 }
