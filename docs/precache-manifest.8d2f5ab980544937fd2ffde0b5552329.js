aa9f51cbd8608c90d872d445f9e03c210993ed40
LJEsp <esparragoleandroj@gmail.com>
2019-01-15 18:27:37 +0800

    Fix gh

---
+++ docs/precache-manifest.8d2f5ab980544937fd2ffde0b5552329.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "5587b44056a614c505ea",
+    "url": "/study-text-crop/static/js/runtime~main.5587b440.js"
+  },
+  {
+    "revision": "cc168485ee829f72c4f1",
+    "url": "/study-text-crop/static/js/main.cc168485.chunk.js"
+  },
+  {
+    "revision": "8f27fd65e0a55583d929",
+    "url": "/study-text-crop/static/js/1.8f27fd65.chunk.js"
+  },
+  {
+    "revision": "0ba9c59bee6394c0661711232d7c6254",
+    "url": "/study-text-crop/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bf04408193c45141d8b64c0ef6c3ce60.js"
+  "/study-text-crop/precache-manifest.8d2f5ab980544937fd2ffde0b5552329.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/study-text-crop/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
